df128e9e35a8047d531ad33605c6d88526e85d85
ynotawino <ynotomondi57@gmail.com>
2021-04-19 08:21:23 +0300

    Change from using local mongodb to hosted mongodb

--- helper/db.js
+++ helper/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+//Config Files
+require('dotenv').config();
 
 module.exports = () => {
-    const url = 'mongodb://localhost:27017';
     const dbName= 'student-wallet';
-    mongoose.connect(`${url}/${dbName}`, {useNewUrlParser:true});
+    const user=process.env.MONGO_USER;
+    const pass=process.env.MONGO_PASS;
+    console.log("Username is "+user+" and password is "+pass);
+    const url=`mongodb+srv://${encodeURIComponent(user)}:${encodeURIComponent(pass)}@cluster0.8tdwz.mongodb.net/${encodeURIComponent(dbName)}?retryWrites=true&w=majority`
+    //const url = `mongodb://localhost:27017/${dbName}`;
+    mongoose.connect(`${url}`, {useNewUrlParser:true});
     mongoose.connection.on('open', () => {
         console.log('MongoDb: Connected');
     });
