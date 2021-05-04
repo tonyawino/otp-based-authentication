1b51f92316b68ec8710ae27fcecb076ff8107028
tonyawino <ynotomondi57@gmail.com>
2021-05-04 11:38:57 +0300

    Remove user credentials from env and db

--- helper/db.js
+++ helper/db.js
@@ -6,7 +6,7 @@ module.exports = () => {
     const dbName= 'student-wallet';
     const user=process.env.MONGO_USER;
     const pass=process.env.MONGO_PASS;
-    const url=`mongodb+srv://${encodeURIComponent(user)}:${encodeURIComponent(pass)}@cluster0.8tdwz.mongodb.net/${encodeURIComponent(dbName)}?retryWrites=true&w=majority`
+    const url=`mongodb+srv://${encodeURIComponent(user)}:${encodeURIComponent(pass)}@cluster.0dwz.mongodb.net/${encodeURIComponent(dbName)}?retryWrites=true&w=majority`
     //const url = `mongodb://localhost:27017/${dbName}`;
     mongoose.connect(`${url}`, {useNewUrlParser:true});
     mongoose.connection.on('open', () => {
